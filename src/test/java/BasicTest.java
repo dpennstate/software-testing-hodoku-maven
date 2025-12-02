9cea7ce763f3eca388311f8ea19e36781285cc29
Danny Skwarcha <dts5222@psu.edu>
2025-12-01 23:05:38 -0500

    Initial commit

---
+++ src/test/java/BasicTest.java
@@ -0,0 +1,9 @@
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+public class BasicTest {
+
+    @Test
+    void testSomething() {
+        assertTrue(true);
+    }
+}

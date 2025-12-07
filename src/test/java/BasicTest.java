9daf200f91fb67f2ce5db57b2ee61f9aa926af27
Danny Skwarcha <dts5222@psu.edu>
2025-12-07 16:43:44 -0500

    Removed BasicTest.java file

--- src/test/java/BasicTest.java
+++
@@ -1,9 +0,0 @@
-import static org.junit.jupiter.api.Assertions.*;
-import org.junit.jupiter.api.Test;
-public class BasicTest {
-
-    @Test
-    void testSomething() {
-        assertTrue(true);
-    }
-}
